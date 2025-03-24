f0cf66bb14fe0b38efd15162bd03cc760efe49f0
TangHui <tanghui.fcn@yahoo.com>
2025-03-24 20:58:33 +0900

    Initial Commit

---
+++ Kineo/KineoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KineoApp.swift
+//  Kineo
+//
+//  Created by 唐惠 on 2025/03/24.
+//
+
+import SwiftUI
+
+@main
+struct KineoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KineoTests/KineoTests.swift
@@ -0,0 +1,17 @@
+//
+//  KineoTests.swift
+//  KineoTests
+//
+//  Created by 唐惠 on 2025/03/24.
+//
+
+import Testing
+@testable import Kineo
+
+struct KineoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
